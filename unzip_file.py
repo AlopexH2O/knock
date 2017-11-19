55c6701255d66aae4803d1f8270dd1e538a651fe
zhenghao <zhengh@nrec.com>
2017-11-19 10:07:32 +0800

    modify unzip_file func

--- unzip_file.py
+++ unzip_file.py
@@ -1,19 +1,35 @@
 import subprocess
+import os
 
+def unzip_file(filepath, destpath = "./", filesign = "*"):
+	#   return value:
+	#	res: success(0) or fail
+	#   err: error information
+	#	file: the dest file list
+	#
+	file_list = []
+	cmd = r"7z e -y " + filepath + r" -o" + destpath + r" " + filesign + r" -r"
+	p = subprocess.Popen(cmd, shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+	try:
+		out, err = p.communicate(timeout = 15)
+	except TimeoutExpired:
+		p.kill()
+		out, err = p.communicate()
+	res = p.returncode
+	flist = os.listdir(destpath)
+	ndir = os.path.abspath(destpath)
+	for i in flist:
+		file_list.append(os.path.join(ndir, i))
+	return res, err, file_list
 
-def unzip_file(filepath, destpath, filesign):
-	cmd = r"7z e " + filepath + r" -o" + destpath + r" " + filesign + r" -r"
-	p = subprocess.Popen(cmd, shell = True, stdout = subprocess.PIPE)
-	str = p.stdout.read()
-	res = p.wait()
-	return res, str
 
 
 
 if __name__ == '__main__':
 	file = r"G:\PCS_PROJECT\_TEST\PCS_992_TEST\aaa.7z"
 	dest = r".\dest"
-	sign = r"*.dev"
-	rt, content = unzip_file(file, dest, sign)
+	sign = r"*.de"
+	rt, err, content = unzip_file(file, dest, sign)
 	print("rt = ", rt)
-	print(content.decode())
+	print("err = ", err.decode())
+	print("content = ", content)
